5c6475da179c2259befe9bf4ebc11cafb2f0c106
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-09-23 00:04:07 -0700

    Initialize a new React application with Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Halal Restaurants In Dallas, TX</h1>
+      <h2>Welcome to the best foodgit!</h2>
+      
+    </div>
+  )
+}
+
+
+export default App
